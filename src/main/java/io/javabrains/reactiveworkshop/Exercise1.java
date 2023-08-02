3d24c9ba8f600d6049c30c36769a985db176e0b3
Möhsün Babayev <mohsun.babayev@protonmail.com>
2023-08-02 21:02:23 +0400

    exercise 1 done

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -7,23 +7,42 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .forEach(System.out::println);
+        System.out.println("=".repeat(40));
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(i -> i < 5)
+                .forEach(System.out::println);
+        System.out.println("=".repeat(40));
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+                .filter(i -> i > 5)
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
+        System.out.println("=".repeat(40));
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+        System.out.println(StreamSources.intNumbersStream()
+                .filter(i -> i > 5)
+                .findFirst()
+                .orElse(-1));
+        System.out.println("=".repeat(40));
 
         // Print first names of all users in userStream
-        // TODO: Write code here
+        StreamSources.userStream()
+                .forEach(user -> System.out.println(user.getFirstName()));
+        System.out.println("=".repeat(40));
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+        StreamSources.userStream()
+                .filter(user -> StreamSources.intNumbersStream().toList().contains(user.getId()))
+                .forEach(user -> System.out.println(user.getFirstName()));
+        System.out.println("=".repeat(40));
 
     }
 
